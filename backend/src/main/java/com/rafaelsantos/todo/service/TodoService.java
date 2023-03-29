a2696d0f71e736d0b4a28544df0087b24941a5f3
Rafael Santos <rafa.05125@gmail.com>
2023-03-29 02:28:40 -0300

    FindById

---
+++ backend/src/main/java/com/rafaelsantos/todo/service/TodoService.java
@@ -0,0 +1,21 @@
+package com.rafaelsantos.todo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rafaelsantos.todo.domain.Todo;
+import com.rafaelsantos.todo.repository.TodoRepository;
+
+@Service
+public class TodoService {
+	
+	@Autowired
+	private TodoRepository repository;
+	
+	public Todo findById(Integer id) {
+		Optional<Todo> obj = repository.findById(id);
+		return obj.orElse(null);
+	}
+}
