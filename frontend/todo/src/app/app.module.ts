a202f8b3419196350d6bd54fe115c7fe2a70ea3e
Rafael Santos <rafa.05125@gmail.com>
2023-04-05 09:25:02 -0300

    Read all component

--- frontend/todo/src/app/app.module.ts
+++ frontend/todo/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
@@ -7,10 +8,14 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { FooterComponent } from './components/footer/footer.component';
+import { ReadAllComponent } from './components/read-all/read-all.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatToolbarModule],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, ReadAllComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatToolbarModule, MatCardModule, MatButtonModule, MatIconModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
